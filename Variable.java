712eea0a8cf7e1f9106a3e78df011b1bb13d5464
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-22 21:42:08 +0530

    initially commit

---
+++ Variable.java
@@ -0,0 +1,10 @@
+class Variable
+{
+    public static void main(String a[])
+    {
+        int num1=12;
+        int num2=33;
+        int num3=num1 + num2;
+        System.out.println("addition:  "+num3);
+    }
+}
