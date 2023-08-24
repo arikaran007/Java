f6634a42dd045e9e0b492ae8f409efb5ff521d9f
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-24 20:28:50 +0530

    primitive datatype

---
+++ DataType.java
@@ -0,0 +1,18 @@
+class DataType
+{
+    public static void main(String a[])
+    {
+        byte num1 = 122;  //1 byte -128 to 127
+        int  num2 = 233; //4 byte 
+        long num3 = 2332l; //8 byte
+        short  num4 = 233; //2 byte
+
+        double num5 = 23.34; //8 byte
+        float num6 = 23.34f; //4 byte
+
+        char c ='k';  //2 byte
+
+        boolean b = true ; //1 bit
+
+    }
+}
