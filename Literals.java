813fd1eb6552e43d5379c4cdc0ec8a80be75571c
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-24 20:43:07 +0530

    5 literals
    Boolean (boolean), integer (int), character (char), string (string), and floating-point (float and double) literals

---
+++ Literals.java
@@ -0,0 +1,24 @@
+class Literals {
+    public static void main(String a[])
+    {
+       /*binary 
+        int num = 0b101;
+         System.out.println(num);*/
+/*hexadecimal 
+        int  num = 0x7E;
+        System.out.println(num) ;*/
+
+      /*   int num=100_00_00_000;
+        System.out.println(num);
+        o/p is 10000000*/
+
+        /*double num=69;
+        System.out.println(num);
+        o/p: 69.0 
+         */
+
+         char c ='a';
+         c++;
+         System.out.println(c);
+    }
+}
