e3d49697099191209d0f6c93285bd07b1c8829d0
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-25 06:53:44 +0530

    widening and narrowing

---
+++ TypeCasting.java
@@ -0,0 +1,22 @@
+class TypeCasting
+{
+    public static void main(String a[])
+    {
+        //widening
+  /*       byte b=127;
+        int  c=233;
+
+        c=b;
+        System.out.println(c);    */
+
+//narrowing
+        float f = 4.6f;
+        int num =424;
+        num=(int)f;
+
+        System.out.println(num);
+
+
+
+    }
+}
