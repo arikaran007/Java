13591ba917a6b0d1b3c568581144450956bb2325
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-26 07:57:28 +0530

    comparision

---
+++ Relational.java
@@ -0,0 +1,33 @@
+class Relational
+{
+    public static void main(String  a[])
+    {
+        int x = 5;
+        int y = 4;
+         
+      //  boolean result = x<y;
+     //   boolean result = x <=y;
+        boolean result  = x!=y;
+        
+        System.out.println(result);
+
+        double num = 4.3;
+        double num1 = 3.24;
+         
+      //  boolean result1 = num > num1;
+
+//        boolean result1 = num>= num1;
+        boolean result1 = num == num1 ;   
+
+        System.out.println(result1);
+    }
+}
+
+
+/*   x > y;
+      x < y;
+      x <= y ;
+      x >=y;
+      x == y;
+      x !=y;
+ */
