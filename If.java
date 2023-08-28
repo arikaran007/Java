a6566e884d1b719cc6af3be8ea71d35209293538
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-28 06:44:58 +0530

    ifelse

---
+++ If.java
@@ -0,0 +1,16 @@
+public class If {
+    public static void main(String a[])
+    {
+        int x=10;
+        int y=34;
+
+        if(x>y && x<10)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
