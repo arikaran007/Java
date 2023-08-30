b79f6482c0a5f659b689b32163690e8ad1791405
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-30 07:04:23 +0530

    execute one time if false

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile {
+    public  static void main(String a[])
+    {
+      //  int i=1;
+        int i=6;
+        do{
+            System.out.println("Ari " + i);
+            i++;
+
+        }while(i<6);
+    }
+}
