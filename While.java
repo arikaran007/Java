61b4c987ac83244a2790261a60641eedd8ba3e73
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-08-30 06:53:28 +0530

    loop

---
+++ While.java
@@ -0,0 +1,19 @@
+class While
+{
+    public static void main(String a[])
+    {
+        int i=1;
+       
+        while(i<6)
+        {
+            System.out.println("ari");
+            int j=1;
+            while(j<3)
+            {
+                System.out.println("karan");
+                j++;
+            }
+            i++;
+        }
+    }
+}
