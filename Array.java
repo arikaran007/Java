0493acc44af75c72af4a9fa6c38a5d76b2e57457
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-09-05 07:01:22 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,19 @@
+class Array{
+    public static void main(String a[])
+    {
+       // int nums[] = {3,7,8};
+
+       // System.out.println(nums[1]);
+
+        int nums[] = new int[4];
+        nums[0]=3;
+        nums[1]=2;
+        nums[2]=6;
+        nums[3] = 9;
+
+        for(int i=0;i<=3;i++)
+        {
+            System.out.println(nums[i]);
+        }
+    }
+}
