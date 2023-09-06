cf5983c84e72fdfb0a3d6c20d3b5655bf4e20a61
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-09-06 12:12:31 +0530

    Array of Object

---
+++ OArray.java
@@ -0,0 +1,41 @@
+class Students{
+    int rollno;
+    String name;
+    int marks;
+}
+public class OArray {
+    public static void main(String a[])
+    {
+        Students s1 = new Students();
+        s1.rollno = 23;
+        s1.name ="ari";
+        s1.marks = 90;
+
+        Students s2 = new Students();
+        s2.rollno = 24;
+        s2.name ="karan";
+        s2.marks = 98;
+
+        Students s3 = new Students();
+        s3.rollno = 27;
+        s3.name ="rockt";
+        s3.marks = 93;
+
+
+        Students student[] = new Students[3];
+        student[0]= s1;
+        student[1]=s2;
+        student[2]=s3;
+
+
+
+        for(int i=0;i<student.length;i++)
+        {
+            System.out.println(student[i].name + ":" + student[i].marks);
+        }
+    }
+    
+
+}
+    
+
