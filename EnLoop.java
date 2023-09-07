8cb9a16478aedd4d39e9d51a0752c902b17ced72
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-09-07 07:05:05 +0530

    Enhanced loop

---
+++ EnLoop.java
@@ -0,0 +1,14 @@
+public class EnLoop {
+    public static void main(String[] args) {
+        int num[]=new int[3];
+        num[0]=7;
+        num[1]=2;
+        num[2]=3;
+
+        for(int n : num)
+        {
+            System.out.println(n);
+                
+        }
+    }
+}
