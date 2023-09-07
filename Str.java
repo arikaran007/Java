3bafa057f551087a893c50cb055887253f051778
Arikaran S <124378874+arikaran007@users.noreply.github.com>
2023-09-07 07:06:08 +0530

    String

---
+++ Str.java
@@ -0,0 +1,10 @@
+public class Str {
+    public static void main(String[] args)
+    {
+        String name ="ari";
+        // we using like this String name = int String("ari");
+
+        System.out.println("Hello " + name);
+
+    }
+}
